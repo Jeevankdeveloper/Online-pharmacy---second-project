80d546978d85a5960d281a3630d51e53d630d0a1
Jeevankdeveloper <jeevan.siripurapu@gmail.com>
2022-10-07 11:39:38 +0530

    coupon

---
+++ index.js
@@ -0,0 +1,20 @@
+//Coupon Function
+
+function loadCoupon() {
+    document.getElementById('coupon').style.visibility = "visible";
+    document.getElementById('bodyInside').style.opacity = "0.7";
+}
+
+
+
+function closeCoupon() {
+    document.getElementById('coupon').style.visibility = "hidden";
+    document.getElementById('bodyInside').style.opacity = "1";
+}
+
+//Change color
+
+function changeMode() {
+    let mybody = document.body;
+    mybody.classList.toggle('mydark');
+}
